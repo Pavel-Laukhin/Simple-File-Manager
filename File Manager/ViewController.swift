ccbe42405acff8b8da6ccf88d9d41c13082bdb96
Pavel Laukhin <6886834@gmail.com>
2020-12-21 14:17:30 +0300

    Initial Commit

---
+++ File Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  File Manager
+//
+//  Created by Павел on 21.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
